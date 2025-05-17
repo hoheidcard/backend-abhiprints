a7a2e48288eb8fb19949eca288444ca94d4e36aa
ankitjain0748 <ankitjain0748@gmail.com>
2025-05-17 23:32:54 +0530

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
+import express from 'express';
 import * as compression from 'compression';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
