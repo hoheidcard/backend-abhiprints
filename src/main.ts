7d533c6a46229dc216bad524522749db9aec0bc2
ankitjain0748 <ankitjain0748@gmail.com>
2025-05-17 23:06:20 +0530

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,54 +1,17 @@
-// import compression from '@fastify/compress';
-// import fastifyCsrf from '@fastify/csrf-protection';
-// import helmet from '@fastify/helmet';
-// import {
-//   FastifyAdapter,
-//   NestFastifyApplication,
-// } from '@nestjs/platform-fastify';
-// import * as fastifyMultipart from 'fastify-multipart';
-
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
 import * as compression from 'compression';
-
-// import cluster from 'cluster';
-// import os from 'os';
-
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { readFileSync } from 'fs';
 import { AppModule } from './app.module';
 
+const server = express();
+
 async function bootstrap() {
-  // const numCPUs = os.cpus().length;
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
-  // if (cluster.isPrimary) {
-  //   // Fork workers
-  //   for (let i = 0; i < numCPUs; i++) {
-  //     cluster.fork();
-  //   }
-  //   cluster.on('exit', (worker, code, signal) => {
-  //     console.log(`Worker ${worker.process.pid} died`);
-  //     // Restart the worker if it exits
-  //     cluster.fork();
-  //   });
-  // } else {
-
-  // const httpsOptions = {
-  //   key: readFileSync('ssl/private.pem'),
-  //   cert: readFileSync('ssl/certificate.crt'),
-  //   ca: readFileSync('ssl/sslca.ca-bundle'),
-  // };
-  // const app = await NestFactory.create(AppModule/*, { httpsOptions }*/);
-  // app.getHttpAdapter().getInstance().server.setMaxListeners(0); // Set maximum listeners to unlimited
-
-  // const app = await NestFactory.create<NestFastifyApplication>(
-  //   AppModule,
-  //   new FastifyAdapter({
-  //     // https: httpsOptions,
-  //   }),
-  // );
-
-  const app = await NestFactory.create(AppModule);
+  // Swagger API Docs Setup
   const config = new DocumentBuilder()
     .setTitle('HariOM Server')
     .setDescription('The HariOM server API description')
@@ -56,19 +19,19 @@ async function bootstrap() {
     .addTag('HariOM Server')
     .addBearerAuth()
     .build();
-
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  // Global Configurations
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   app.enableCors();
   app.use(compression());
-  // app.use(fastifyMultipart());
-  // await app.register(compression);
-  // await app.register(helmet);
-  // await app.register(fastifyCsrf);
-  await app.listen(3111);
-  // }
+
+  await app.init();
 }
+
 bootstrap();
+
+// Export `server` for Vercel handling
+export default server;
