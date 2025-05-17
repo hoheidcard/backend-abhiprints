36048cf9700e5e922719a290da98231bdff9e0c4
ankitjain0748 <ankitjain0748@gmail.com>
2025-05-18 01:16:07 +0530

    error bug

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,26 +1,15 @@
-// import { Controller, Get } from '@nestjs/common';
-// import { AppService } from './app.service';
-
-// @Controller()
-// export class AppController {
-//   constructor(private readonly appService: AppService) {}
-
-//   @Get()
-//   getHello(): string {
-//     return this.appService.getHello();
-//   }
-// }
-
-
-
 import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
 
-
-@Controller() // Ensures the route is under `/api/v1`
+@Controller()
 export class AppController {
-  @Get('health')
-  checkHealth(): string {
-    return 'API is running smoothly!';
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
   }
 }
+
+
 
